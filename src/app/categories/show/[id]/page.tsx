f94edf30bff77f6be2d6e48daf7132791a8422df
supereact <supereact@outlook.com>
2024-07-01 20:25:00 -0400

    Initial commit from create-refine-app

---
+++ src/app/categories/show/[id]/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Stack, Typography } from "@mui/material";
+import { useShow } from "@refinedev/core";
+import { Show, TextFieldComponent as TextField } from "@refinedev/mui";
+
+export default function CategoryShow() {
+  const { queryResult } = useShow({});
+  const { data, isLoading } = queryResult;
+
+  const record = data?.data;
+
+  return (
+    <Show isLoading={isLoading}>
+      <Stack gap={1}>
+        <Typography variant="body1" fontWeight="bold">
+          {"ID"}
+        </Typography>
+        <TextField value={record?.id} />
+        <Typography variant="body1" fontWeight="bold">
+          {"Title"}
+        </Typography>
+        <TextField value={record?.title} />
+      </Stack>
+    </Show>
+  );
+}
